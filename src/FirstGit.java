34e835d2fb7d7d43374112f8469543ed70c94532
hudson <sundari@vaavel.com>
2014-09-19 15:10:43 +0530

    commit

--- src/FirstGit.java
+++ src/FirstGit.java
@@ -3,7 +3,8 @@ public class FirstGit {
 	
 	public static void main(String args[])
 	{
-		System.out.println("hai rajkumar");
+		System.out.println("hai raj");
+		System.out.println("Welcome Hudson");
 	}
 
 }
